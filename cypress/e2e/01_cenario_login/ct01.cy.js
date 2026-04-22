12269f0aa5578cb99e96dc7a4b1700f0fe20f0f8
pcdajr <pcdajr@gmail.com>
2026-04-22 03:06:23 -0300

    organização da estrutura dom

--- cypress/e2e/01_cenario_login/ct01.cy.js
+++ cypress/e2e/01_cenario_login/ct01.cy.js
@@ -4,6 +4,6 @@ describe('Página de login Saucedemo', () => {
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
-    //teste de commit
+    
   })
 }) 
