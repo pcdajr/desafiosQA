b64f16d101027e72fd6ca7d505fd3a197df1ad86
pcdajr <pcdajr@gmail.com>
2026-04-22 02:55:16 -0300

    Organização de pastas do projeto e automação do ct01 criado

---
+++ cypress/e2e/01_cenario_login/ct01.cy.js
@@ -0,0 +1,9 @@
+describe('Página de login Saucedemo', () => {
+  it('Deve carregar a página do login/Home', () => {
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.get('[data-test="login-button"]').click();
+
+  })
+}) 
