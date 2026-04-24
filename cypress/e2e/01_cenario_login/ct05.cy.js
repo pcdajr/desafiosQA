d1681d26456f8e0dc7e9d5dee8993586bbe186a9
pcdajr <pcdajr@gmail.com>
2026-04-24 19:21:11 -0300

    finalizando cenário 01 - login

--- cypress/e2e/01_cenario_login/ct05.cy.js
+++ cypress/e2e/01_cenario_login/ct05.cy.js
@@ -5,10 +5,15 @@ describe('Página de login Saucedemo', () => {
   });
 
   it('Usuário Fake - sem cadastrado no sistema', () => { 
-   cy.get('[data-test="username"]').type("paulo");
+    cy.get('[data-test="username"]').type("paulo");
     cy.get('[data-test="password"]').type("1234");
     cy.get('[data-test="login-button"]').click();
-     
+
+    // capturando o elemento, verificando se tem a mensagem e se é vísivel.
+
+    cy.contains('[data-test="error"]',"Usuário não existe, insira um válido")  
+      .should('be.visible');
+                                     
     
   })
 }) 
