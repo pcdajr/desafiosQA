1a8c3bc8f9fe8240fc82baeffdc287d6f08c2ab1
pcdajr <pcdajr@gmail.com>
2026-04-25 17:24:47 -0300

    Correções de configuração do cypress

--- cypress/e2e/01_cenario_login/ct01.cy.js
+++ cypress/e2e/01_cenario_login/ct01.cy.js
@@ -5,10 +5,12 @@ describe('Página de login Saucedemo', () => {
     cy.visit('/'); // antes do caso teste, visitando a url.
   });
 
-  it('Login válido', () => {
-    cy.get('[data-test="username"]').type("standard_user");   //inserindo nome
+  it('Login válido mas sem toast notificacion("logado com sucesso")', () => {
+
+    cy.get('[data-test="username"]').type("standard_user");   //inserindo nomeS
     cy.get('[data-test="password"]').type("secret_sauce");    //inserindo senha   
     cy.get('[data-test="login-button"]').click();             //clicando
+
     
   })
 }) 
