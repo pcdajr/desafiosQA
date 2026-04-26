2e693f7201d4988740af88a41f65e4470d6df314
pcdajr <pcdajr@gmail.com>
2026-04-26 01:18:19 -0300

    Resultados mas falta tratamento do ct06 e continuar os 2 cenarios restantes

--- cypress/e2e/_login/ct06_sob_perfomance.cy.js
+++ cypress/e2e/_login/ct06_sob_perfomance.cy.js
@@ -9,9 +9,7 @@ describe('Página de login Saucedemo', () => {
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
 
-  
-    //tentando forçar erro de asserção por tempo, nesse usuário lento.
-    cy.get('[data-test="login-button"]',{ timeout: 1000 }).should('not.exist'); 
+   
     
   })
 }) 
