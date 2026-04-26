717e70d5d5fcc4d199a6620d220bef02187cb846
pcdajr <pcdajr@gmail.com>
2026-04-25 22:16:24 -0300

    Configuração do mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "822322",
   allowCypressEnv: false,
 
   e2e: {
@@ -9,6 +10,13 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
       video: true,
+      reporter: 'mochawesome',
+      reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: "mmddyyyy_HHMMss" }
       
   },
 
